151dd16e932e98c6810c135b219f04d2611002cd
Jonathas <jonathaseloi@gmail.com>
2026-04-08 23:58:21 -0300

    add gitignore and github actions deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Troque 'osrs-mission-board' pelo nome exato do seu repositório no GitHub
   base: '/pick-a-mission/',
 })
